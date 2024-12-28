f4a88bdca93cde2fcc7db9665fab23fee07cc167
Khiew Jian Bin <khiewbin@gmail.com>
2024-12-28 22:10:37 +0800

    add my old scripts that i done

---
+++ Assets/AEM/Camera/CameraControlTest.cs
@@ -0,0 +1,29 @@
+﻿using AEM;
+using UnityEngine;
+
+public class CameraControlTest : MonoBehaviour
+{
+    public AEMCameraControl t;
+
+    public void Awake()
+    {
+        if (t == null)
+        {
+            t = GetComponent<AEMCameraControl>();
+        }
+    }   
+    void Start()
+    {
+        if (t == null)
+        {
+            enabled = false;
+            Debug.Log(this.gameObject + " Could Not Find CameraControl script");
+        }
+        else
+        {
+            AEMCamera a = t.GetCam(Random.Range(0, t.Cameralist.Count - 1));
+            t.SetUpCam(0,0,0, Vector3.zero, Vector3.zero,CameraPreset.Chase);
+            a.Cam.enabled = true;
+        }
+    }
+}
---
+++ Assets/AEM/Timer/Example/TimerExample.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class TimerExample : MonoBehaviour
+{
+    //Static Instance - So that Everyone can Access it
+    public static TimerExample instance = null;
+
+    public Timer StartEndTimer;
+    public Timer IntervalTimer;
+
+    void Start()
+    {
+        StartEndTimer = Timer.CreateNew("Timer");
+        StartEndTimer.transform.parent = transform;
+        StartEndTimer.Setup(0, 1f, Timer.TimerType.StartEnd);
+
+        IntervalTimer = Timer.CreateNew("Timer");
+        IntervalTimer.transform.parent = transform;
+        IntervalTimer.Setup(1f, Mathf.Infinity, Timer.TimerType.Interval);
+    }
+     
+    void Update()
+    {
+        if (StartEndTimer.CounterHit)
+        {
+            print("start end hit");
+            StartEndTimer.Reset();
+        }
+        if (IntervalTimer.CounterHit)
+        {
+            print("interval hit");
+        }
+    }
+}
