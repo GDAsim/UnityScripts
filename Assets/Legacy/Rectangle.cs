b23b37838149e8a62f8bd2c1b3749ca3d414ec92
Khiew Jian Bin <khiewbin@gmail.com>
2025-03-02 20:40:45 +0800

    add legacy

---
+++ Assets/Legacy/Rectangle.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Rectangle
+{
+    public float centerX;
+    public float centerY;
+    public float width;
+    public float height;
+
+    public Rectangle(float x, float y, float w, float h)
+    {
+        centerX = x;
+        centerY = y;
+        width = w;
+        height = h;
+    }
+
+    public bool Contains(Vector3 point)
+    {
+        bool contains =
+            point.x > centerX - width &&
+            point.x < centerX + width &&
+            point.y > centerY - height &&
+            point.y < centerY + height;
+
+        return contains;
+    }
+}
